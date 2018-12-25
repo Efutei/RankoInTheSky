c66b6dd51a87cb2d8090d7bae5f032e544c003ca
Efutei <hirofunafumi@gmail.com>
2018-12-25 20:10:12 +0900

    BGMの音量変更と微調整

--- main.js
+++ main.js
@@ -114,6 +114,8 @@ phina.define('MainScene', {
     this.timeText = TimeText().addChildTo(this);
     this.circles = [];
     this.circleCount = 0;
+    SoundManager.setVolume(0.8);
+    SoundManager.setVolumeMusic(0.06);
     SoundManager.playMusic('bgm');
   },
   update: function(app){
@@ -159,7 +161,8 @@ phina.define('MainScene', {
         rankMessage = 'Rank: 制限中';
       }
     }
-    if(time <= -2000){
+    if(time <= -2600){
+      SoundManager.stopMusic();
       this.exit({
         score: score,
         message: rankMessage,
